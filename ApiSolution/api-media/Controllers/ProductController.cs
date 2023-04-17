604fe81fe3de844d72bacc3e53f63fe49822b636
Magdaleno0100 <magdaleno0100@gmail.com>
2023-04-16 23:32:53 -0600

    Add commit for producto controller

---
+++ ApiSolution/api-media/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_media.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class ProductController: ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Get from Producto Controller";
+        }
+    }
+}
