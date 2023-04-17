3a02138af7d3d69b404cffd27ccb956bf3cfd7f7
Magdaleno0100 <magdaleno0100@gmail.com>
2023-04-17 07:02:22 -0600

    Revert "Clean using"
    This reverts commit 4f060e38635458332e0d4e063061aa3d3e2cdffd.

--- ApiSolution/api-media/Data/DataUser.cs
+++ ApiSolution/api-media/Data/DataUser.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using api_media.Models;
 
 namespace api_media.Data
--- ApiSolution/api-media/Repository/UserRepository.cs
+++ ApiSolution/api-media/Repository/UserRepository.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using api_media.Data;
 using api_media.Models;
 
